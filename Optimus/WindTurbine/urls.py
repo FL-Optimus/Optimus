df985777ad08e19b9ae7eee72a4e058615e037ff
Jens Reichwein <jensreichwein@web.com>
2021-02-27 17:40:49 +0100

    create list view, update detail view, make favicon

--- Optimus/WindTurbine/urls.py
+++ Optimus/WindTurbine/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('airfoils', views.airfoil_list_view, name='airfoils'),
+    path('airfoils/<int:id>', views.airfoil_detail_view, name='airfoil_detail'),
 ]
--- Optimus/WindTurbine/views.py
+++ Optimus/WindTurbine/views.py
@@ -6,18 +6,60 @@ from xhtml2pdf import pisa
 
 from .models import Airfoil
 
+import csv
+
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
-def airfoil_detail_view(request, id):
+def airfoil_list_view(request):
+    try:
+        airfoils = Airfoil.objects.all()[:10]
+    except Airfoil.DoesNotExist:
+        return render(request, '404.html')
+    context = {
+        'airfoils': airfoils
+    }
+
+    return render(request, 'airfoil_list.html', context)
+
+def get_airfoil_properties(airfoil):
+    try:
+        x = airfoil.geometry['x']
+        y = airfoil.geometry['y']
+    except TypeError:
+        return None
+
+    end_value = int(round(len(x)/2))
+
+    thickness = [abs(y[i]) + abs(y[-1-i]) for i,j in enumerate(x[:end_value])]
+    camber = [(y[i] + y[-1-i])/2 for i, j in enumerate(x[:end_value])]
+
+    max_thickness = max(thickness)
+    max_thickness_position = x[thickness.index(max_thickness)] * 100
+
+    max_camber = max(camber)
+    max_camber_position = x[camber.index(max_camber)] * 100
+
+    properties = {
+        'thickness': f'{max_thickness * 100 :.2f}',
+        'thickness_position': f'{max_thickness_position :.2f}',
+        'camber': f'{max_camber * 100 :.2f}',
+        'camber_position':  f'{max_camber_position :.2f}',
+    }
+    return properties
+
+
+def airfoil_detail_view(request, id, calc=False):
     try:
         airfoil = Airfoil.objects.get(id=id)
     except Airfoil.DoesNotExist:
         return render(request, '404.html')
 
+    properties = get_airfoil_properties(airfoil)
     context = {
-        'airfoil': airfoil
+        'airfoil': airfoil,
+        'properties': properties,
     }
 
     return render(request, 'airfoil_detail.html', context)

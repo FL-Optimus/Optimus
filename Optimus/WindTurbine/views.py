7c78831e165070b73f9ad22834ac885750804a7f
Jens Reichwein <jensreichwein@web.com>
2021-03-17 15:34:17 +0100

    adapt pdf view

--- Optimus/WindTurbine/views.py
+++ Optimus/WindTurbine/views.py
@@ -9,25 +9,32 @@ from .models import Customer
 def home(request):
     return render(request, 'index.html')
 
+
+from io import BytesIO
 def render_pdf_view(request):
     customer = Customer.objects.first()
+    context = {'customer': customer, 'image': customer.logo.path}
 
-    template_path = 'PDFs/pdf_download5.html'
-    context = {'customer': customer}
-    print(customer.__dict__)
+    template = get_template('PDFs/pdf_download.html')
+    html = template.render(context)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode('ISO-8859-1')), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+    # print(customer.__dict__)
     # Create a Django response object, and specify content_type as pdf
-    response = HttpResponse(content_type='application/pdf')
+    # response = HttpResponse(content_type='application/pdf')
     # if download:
     # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
     # elif display:
-    response['Content-Disposition'] = 'filename="report.pdf"'
+    # response['Content-Disposition'] = 'filename="report.pdf"'
     # find the template and render it.
-    template = get_template(template_path)
-    html = template.render(context)
+
 
     # create a pdf
-    pisa_status = pisa.CreatePDF(html, dest=response)
+    # pisa_status = pisa.CreatePDF(html, dest=response)
     # if error then show some funy view
-    if pisa_status.err:
-       return HttpResponse('We had some errors <pre>' + html + '</pre>')
-    return response
+    # if pisa_status.err:
+    #    return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    # return response
